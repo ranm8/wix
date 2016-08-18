67c97a8f8d6be1d8c7358c2c0a3eab93604a829e
Ran Mizrahi <ran@cocycles.com>
2016-08-18 20:18:47 +0300

    changed file names

--- index.js
+++ index.js
@@ -21,4 +21,4 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-module.exports = require('./lib/Wix.js');
+module.exports = require('./lib/wix.js');
